2af04ff26fa964ebc2f1b90e4c3e6040e26c8a8e
DimiEvans <dimi1997@hotmail.gr>
2020-05-17 12:19:26 +0300

    Update html-processor.py

---
+++ html-processor.py
@@ -0,0 +1,11 @@
+import re
+
+#διάβασμα του αρχείου
+with open('testpage.txt','r',encoding='utf-8') as fp:
+ text=fp.read()
+
+#εξαγωγή και εκτύπωση των τίτλων
+ptitle = '<title>(.+?)</title>'
+result = re.compile(ptitle)
+title = re.findall(result,text)
+print (title)
