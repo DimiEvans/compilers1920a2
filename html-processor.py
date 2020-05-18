345e77cbb18e28287aca28bccfaf32cb2925fd54
DimiEvans <dimi1997@hotmail.gr>
2020-05-18 20:19:17 +0300

    Update html-processor.py

--- html-processor.py
+++ html-processor.py
@@ -1,3 +1,5 @@
+#Παππά Δήμητρα Π2015015
+
 #χρήση βιβλιοθήκης re
 import re
 
@@ -6,7 +8,26 @@ with open('testpage.txt','r',encoding='utf-8') as fp:
  text=fp.read()
 
 #εξαγωγή και εκτύπωση των τίτλων
- ptitle = '<title>(.+?)</title>'
- result = re.compile(ptitle)
- title = re.findall(result,text)
- print (title)
+  reTitle=re.compile('<title>(.+?)</title>')
+  title=re.findall(reTitle,text)
+  print(title)
+  
+ #Διαγραφή σχολίων
+  reComment=re.compile('<!--(.*?)-->',re.DOTALL)
+  text=reComment.sub(' ',text)
+  
+ #Διαγραφή script και style
+  ScriptStyle=re.compile(r'<script(.*?)</script>|style='(.*?),re.DOTALL)
+  text=ScriptStyle.sub(' ',text)
+ 
+ #Ιδιότητα href
+  href=re.compile(r'<a.+?href="(.*?)".*?>(.*?)</a>',re.DOTALL)
+  text=href.sub(' ',text)
+  
+ #Διαγραφή των ετικετών tags\
+  tags=re.compile(r'<.+?>(.*?)</.+?>',re.DOTALL)
+  text=tags.sub(' ',text)
+  
+  
+  
+  
